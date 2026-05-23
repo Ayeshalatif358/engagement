2eed7bc3feded33ed3289b4353289329b629439a
Ayeshalatif726 <ayeshalatif9778@gmail.com>
2026-05-23 17:21:03 +0500

    connected deployed backend

--- Engagement/settings.py
+++ Engagement/settings.py
@@ -137,7 +137,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "https://your-vercel-app.vercel.app",
+]
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
