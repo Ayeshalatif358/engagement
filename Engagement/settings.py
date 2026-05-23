2f4324561405e4821b8246aa4d95ebf63623a1a4
Ayeshalatif726 <ayeshalatif9778@gmail.com>
2026-05-23 19:34:50 +0500

    engagement

--- Engagement/settings.py
+++ Engagement/settings.py
@@ -86,14 +86,13 @@ WSGI_APPLICATION = 'Engagement.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-import dj_database_url
 import os
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default='sqlite:///db.sqlite3',
-        conn_max_age=600
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
